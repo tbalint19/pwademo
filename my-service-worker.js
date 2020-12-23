1885c288d42473cc7f6cdac316e0118f338a6c99
Tóth <tothbalint719@gmail.com>
2020-12-23 17:33:42 +0100

    pwademo

--- my-service-worker.js
+++ my-service-worker.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function(event) {
           '/pwademo/js/calculate.js',
           '/pwademo/index.html'
         ]
-      ).then(r => caches.open('my-static-cache').then((c)=console.log(c))));
+      ).then(r => caches.open('my-static-cache').then((c)=>console.log(c)));
     })
   );
 });
