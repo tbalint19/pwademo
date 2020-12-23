f60ab5933ae1f7c86e858ebde40ab5d73bb44fbe
Tóth <tothbalint719@gmail.com>
2020-12-23 15:17:43 +0100

    pwademo

--- my-service-worker.js
+++ my-service-worker.js
@@ -1,16 +1,7 @@
-let x = 5
-let y
-let z
-
-const myF = () => console.log("declared myf!!!")
-
 self.addEventListener('install', function(event) {
-    console.log("installed!!!!");
-    y = 6
+    console.log("installed");
 });
 
 self.addEventListener('activate', function(event) {
-  console.log("runs!!!!");
-  z = 7
-  setInterval(() => console.log("haho"), 1000)
+  console.log(self);
 });
