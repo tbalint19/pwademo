e0e14f66a97a9e9aabfb7d6a355abc3773667c84
Tóth <tothbalint719@gmail.com>
2020-12-23 21:18:10 +0100

    pwademo

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('my-service-worker.js')
   .then(function(registration) {
     console.log('Registration successful, scope is:', registration.scope);
-    registration.update()
+    //registration.update()
   })
   .catch(function(error) {
     console.log('Service worker registration failed, error:', error);
