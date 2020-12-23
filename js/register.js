b6e6e339f7b47e56b05268f974c431006f73e348
Tóth <tothbalint719@gmail.com>
2020-12-23 21:13:44 +0100

    pwademo

--- js/register.js
+++ js/register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pwademo/my-service-worker.js')
+  navigator.serviceWorker.register('my-service-worker.js')
   .then(function(registration) {
     console.log('Registration successful, scope is:', registration.scope);
     registration.update()
--- my-service-worker.js
+++ my-service-worker.js
@@ -1,24 +1,17 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('my-static-cache').then(function(cache) {
-      console.log("cache", cache);
-      return cache.add(
-          'index.html'
-      ).then(r => caches.open('my-static-cache').then((c)=>console.log(c))).catch(e => console.log(e));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('my-static').then(cache => {
+      return cache.addAll([
+        "./",
+      ])
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(e) {
-      console.log("error", e);
-      console.log("event", event);
-      return caches.match(event.request);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request)
     })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  console.log("self", self);
-});
+  )
+})
