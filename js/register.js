5c58eefece0b787c645c1e1a0d8eaa25fed4a970
Tóth <tothbalint719@gmail.com>
2020-12-23 14:00:51 +0100

    pwademo

--- js/register.js
+++ js/register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js', {
+  navigator.serviceWorker.register('/my-service-worker.js', {
     scope: '/pwademo'
   })
   .then(function(registration) {
