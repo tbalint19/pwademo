bebbe7d675f40adadbb870fa98e56ab23d260aa5
Tóth <tothbalint719@gmail.com>
2020-12-23 13:58:58 +0100

    pwademo

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,16 @@
+let x = 5
+let y
+let z
+
+const myF = () => console.log("declared myf!!!")
+
 self.addEventListener('install', function(event) {
     console.log("installed!!!!");
+    y = 6
 });
 
 self.addEventListener('activate', function(event) {
   console.log("runs!!!!");
-  setInterval(() => console.log("haho"))
+  z = 7
+  setInterval(() => console.log("haho"), 1000)
 });
