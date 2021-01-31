5d24be0bb757a6843da73a8b382c8d5c9398d52e
Tóth <tothbalint719@gmail.com>
2021-01-31 16:38:56 +0100

    no scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+window.addEventListener("scroll", preventMotion, false);
+window.addEventListener("touchmove", preventMotion, false);
+
+function preventMotion(event)
+{
+    window.scrollTo(0, 0);
+    event.preventDefault();
+    event.stopPropagation();
+}
+
 var app = new Vue({
   el: '#app',
   data: {
